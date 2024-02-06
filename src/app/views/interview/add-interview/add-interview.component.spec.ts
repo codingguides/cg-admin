a6d5adffc7506dcdb648c4da9725b6f7541fe6dc
avtarSuman95 <naskarsuman195@gmail.com>
2024-02-06 11:08:48 +0530

    resolve error

--- src/app/views/interview/add-interview/add-interview.component.spec.ts
+++ src/app/views/interview/add-interview/add-interview.component.spec.ts
@@ -2,11 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import { ButtonModule, CardModule, FormModule, GridModule } from '@coreui/angular';
+import {
+  ButtonModule,
+  CardModule,
+  FormModule,
+  GridModule,
+} from '@coreui/angular';
 import { IconSetService } from '@coreui/icons-angular';
 import { iconSubset } from '../../../icons/icon-subset';
 import { DocsComponentsModule } from '../../../../components';
-import { AddTopicComponent } from './add-topic.component';
+import { AddTopicComponent } from '../../topic/add-topic/add-topic.component';
 
 describe('AddTopicComponent', () => {
   let component: AddTopicComponent;
@@ -16,10 +21,17 @@ describe('AddTopicComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AddTopicComponent],
-      imports: [CardModule, GridModule, FormsModule, FormModule, ButtonModule, DocsComponentsModule, RouterTestingModule],
-      providers: [IconSetService]
-    })
-      .compileComponents();
+      imports: [
+        CardModule,
+        GridModule,
+        FormsModule,
+        FormModule,
+        ButtonModule,
+        DocsComponentsModule,
+        RouterTestingModule,
+      ],
+      providers: [IconSetService],
+    }).compileComponents();
   });
 
   beforeEach(() => {
--- src/app/views/question/question-list/question-list.component.spec.ts
+++ src/app/views/question/question-list/question-list.component.spec.ts
@@ -2,11 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import { ButtonModule, CardModule, FormModule, GridModule } from '@coreui/angular';
+import {
+  ButtonModule,
+  CardModule,
+  FormModule,
+  GridModule,
+} from '@coreui/angular';
 import { IconSetService } from '@coreui/icons-angular';
 import { iconSubset } from '../../../icons/icon-subset';
 import { DocsComponentsModule } from '../../../../components';
-import { TopicListComponent } from './topic-list.component';
+import { TopicListComponent } from '../../topic/topic-list/topic-list.component';
 
 describe('TopicComponent', () => {
   let component: TopicListComponent;
@@ -16,10 +21,17 @@ describe('TopicComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TopicListComponent],
-      imports: [CardModule, GridModule, FormsModule, FormModule, ButtonModule, DocsComponentsModule, RouterTestingModule],
-      providers: [IconSetService]
-    })
-      .compileComponents();
+      imports: [
+        CardModule,
+        GridModule,
+        FormsModule,
+        FormModule,
+        ButtonModule,
+        DocsComponentsModule,
+        RouterTestingModule,
+      ],
+      providers: [IconSetService],
+    }).compileComponents();
   });
 
   beforeEach(() => {
